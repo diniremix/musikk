d82b8c36b34b446611ce0ec9413935ede1f78f8b
Jorge Brunal <jorge.brunal@gmail.com>
2016-03-03 14:52:32 +1000

    config the url base, the search method

--- musikker.py
+++ musikker.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import argparse, sys, os, urllib2, json
-
+import argparse, sys, os, urllib2, json, urllib
 from collections import namedtuple
 
-URL_BASE = 'https://api.spotify.com/v1/search?q='
+URL_BASE = 'https://api.spotify.com/v1/search?'
 
 if sys.platform.startswith('linux'):
     os.system('clear')
@@ -14,23 +13,42 @@ elif sys.platform.startswith('win'):
 
 def handleRequest(stream):
     req = stream.read()
-    # resp = json.loads(req)
-
+    # the magic!
     obj = json.loads(req, object_hook=lambda d: namedtuple('musikker', d.keys())(*d.values()))
 
+    print 'Done!'
     print 'Artist:', obj.tracks.items[0].artists[0].name
     print 'Album:', obj.tracks.items[0].album.name
     print 'Track:', obj.tracks.items[0].name
     print 'Spotify uri:', obj.tracks.items[0].uri
 
-request = urllib2.Request(URL_BASE+'track:lacrymosa%20artist:evanescence&type=track&limit=1')
-opener = urllib2.build_opener()
-print 'getting json data...'
+def setParams(query):
+    config = {
+        'q': 'track:'+query['track']+' artist:'+query['artist'],
+        'type': 'track',
+        'limit': query['limit'] or 1
+    }
+    return config
 
-try:
-    stream = opener.open(request)
-    handleRequest(stream)
-except urllib2.HTTPError as e:
-    print 'Error:', e.code, 'the url:', e.url, e.reason
-except urllib2.URLError as e:
-    print 'Error with the url:', e.reason
+def search():
+    query={
+        'artist':'evanescence',
+        'track':'tourniquet',
+        'limit':1
+    }
+
+    params= setParams(query)
+    request= URL_BASE + urllib.urlencode(params)
+
+    print 'searching track: "%s" of Artist: "%s"' % (query['track'], query['artist'])
+
+    try:
+        response = urllib2.urlopen(request)
+        handleRequest(response)
+    except urllib2.HTTPError as e:
+        print 'Error:', e.code, 'the url:', e.url, e.reason
+    except urllib2.URLError as e:
+        print 'Error with the url:', e.reason
+
+print 'getting json data...'
+search()

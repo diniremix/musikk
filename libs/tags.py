5842e94e9932dccec4c2c623989fdb11a236c8b7
Jorge Brunal <jorge.brunal@lateckno.com>
2016-03-19 03:00:27 -0500

    fixed function getMetaData, and create function saveMetaData to saving id3 info

--- libs/tags.py
+++ libs/tags.py
@@ -1,23 +1,47 @@
 import log, os
 from tinytag import TinyTag
 
-def getInfo (name):
-    if os.path.isfile(name):
-        try:
-            tag = TinyTag.get(name)
-            log.warn('tag info:')
-            log.info('Artist:', tag.artist)
-            log.info('Album:', tag.album)
-            log.info('Title:', tag.title)
-            log.info('Track number:', tag.track)
+def saveMetaData (fullname, data, count):
+    log.warn('saving metadata of %s...' % fullname)
+    filename=fullname
+    thefile = open(filename, 'w')
 
-        except Exception as e:
-            log.err("An error occurred while getting metadata of the file:", name)
-            log.err("Error:", e)
-    else:
-        log.err("The file: %s does not exist, check the path or filename" % (name))
+    thefile.write("[musikker]\n")
+    thefile.write("entries=%s\n" % str(count))
 
-def getMetaData(playlists):
+    for item in data:
+        thefile.write("tag=%s\n" % item)
+    pass
+    log.success('metadata saved in:', filename)
+
+def getMetaData(fullname, playlists):
     log.info('accessing metadata...')
+    index = 0
+    tagInfo = []
     for track in playlists:
-        getInfo(playlists[track])
+        name= playlists[track]
+        if os.path.isfile(name):
+            try:
+                filename = os.path.basename(name)
+                log.success('-------------------------')
+                tag = TinyTag.get(name)
+                if tag.title != '' or tag.artist != '':
+                    song = str(tag.title+':'+tag.artist)
+                    tagInfo.append(song)
+                    log.warn('tag info:', filename.encode("ascii", "ignore"))
+                    log.info('Artist:', tag.artist)
+                    log.info('Album:', tag.album)
+                    log.info('Title:', tag.title.encode("ascii", "ignore"))
+                    log.info('Track number:', tag.track)
+                    index += 1
+                else:
+                    log.warn('WARN: no id3 info provide')
+
+            except Exception as e:
+                log.err("An error occurred while getting metadata of the file:", name)
+                log.err("Error:", e)
+        else:
+            log.err("The file: %s does not exist, check the path or filename" % (name))
+    print
+    log.err('track processing:', str(index))
+    saveMetaData(fullname, tagInfo, index)
